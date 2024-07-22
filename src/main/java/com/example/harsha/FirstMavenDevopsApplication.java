83d21389d6c575c12ff337f9375bc58f0fcc7786
harshach1201 <harshavardhan.chokkakula@gmail.com>
2024-07-22 13:52:38 +0530

    added initial commit

---
+++ src/main/java/com/example/harsha/FirstMavenDevopsApplication.java
@@ -0,0 +1,13 @@
+package com.example.harsha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstMavenDevopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstMavenDevopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/harsha/FirstMavenDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.harsha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstMavenDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
